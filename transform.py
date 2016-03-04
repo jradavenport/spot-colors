b6eb62cc785896eaf7f1944bf7a4410a4723b363
James Davenport <jradavenport@gmail.com>
2016-03-04 10:53:14 -0800

    start outlining functions to write

--- transform.py
+++ transform.py
@@ -10,15 +10,36 @@ given starspot modulation from Kepler, a spot delta T, and a stellar T_eff
 import numpy as np
 import matplotlib.pyplot as plt
 from astropy.analytic_functions import blackbody_lambda
+import pysynphot
 
 
-def transform(dflux, Fin='kp', Fout='g', Tstar=5777.0, dTspot=500.):
+def measure_ampl(flux, Tstart=5777.0, dTspot=500.0):
+    '''
+    Given a Kepler light curve (flux values in fractional flux?), assume stellar & spot temperatures,
+    determine the filling factor of the spot relative to the stellar surface
+
     '''
 
-    :param dmag:
-    :param Fin:
-    :param Fout:
-    :return:
+    # input PHOENIX models
+
+    # input Kepler filter
+    Fdir = 'filters/'
+    Ffile = 'kepler_response_hires1'
+
+    # convolve spot & star models w/ filter
+
+    # convert Delta Flux values in to linear combination of convolved models
+
+    fillingfactor = np.zeros_like(flux)
+
+    return fillingfactor
+
+
+def transform(ffactor, Tstar=5777.0, dTspot=500.):
+    '''
+    Given (spot filling factor light curve, stellar & spot temperatures),
+    compute the change in flux in all (ugriz) bands
+
     '''
 
     Fdir = 'filters/'
@@ -28,10 +49,29 @@ def transform(dflux, Fin='kp', Fout='g', Tstar=5777.0, dTspot=500.):
               'kp':Fdir + 'kepler_response_hires1'}
 
 
-    # make BB w/ star Temp
-
-    # make BB w/ spot Temp
-
-    # take input band's flux, convolve output flux to proper band
 
     return outflux
+
+
+def run_LC(file):
+    '''
+    Hand a Kepler light curve (maybe with some pre-processing done)
+
+    Call the needed methods to output ugriz light curves
+    '''
+
+    Tstar = 5000.
+    Tspot = 4500.
+
+    ffactor = measure_ampl(flux, Tstart=Tstar, dTspot=Tstar-Tspot)
+
+    u,g,r,i,z = transform(ffactor, Tstart=Tstar, dTspot=Tstar-Tspot)
+
+    return
+
+
+# how to run code from terminal, like this:
+# $ python transform.py filename.dat
+if __name__ == "__main__":
+    import sys
+    run_LC(sys.argv[1])
